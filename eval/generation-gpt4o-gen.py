4cf700b2dc6e60cc24c142a3933d2574cbca047b
doehyeonlee <139297484+doehyeonlee@users.noreply.github.com>
2024-10-17 16:12:34 +0900

    Update generation-gpt4o-gen.py

--- eval/generation-gpt4o-gen.py
+++ eval/generation-gpt4o-gen.py
@@ -6,14 +6,12 @@ import json
 import logging
 
 # OpenAI API Key
-api_key = "YOUR_OPENAI_API_KEY"
+api_key = "API_KEY"
 client = OpenAI(
     api_key=api_key
 )
 
-
-# 设置日志配置
-logging.basicConfig(filename='./gpt-call/gpt4o-gen/log_file.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='log_file.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 # Function to load data from a JSON file
@@ -35,10 +33,10 @@ def encode_image(image_path):
 
 
 # Function to call an API with an image and a text prompt
-def api_call(prompt, base64_image):
+def api_call(prompt, base64_image, _model_):
     # Assuming client is already initialized and available here
     response = client.chat.completions.create(
-    model="gpt-4o",
+    model=_model_,
     messages=[
         {
         "role": "user",
@@ -61,7 +59,8 @@ def api_call(prompt, base64_image):
 
 forbidden_list = []
 # Function to process a range of data items and save results
-def process_data(data, start_idx, end_idx):
+def process_data(data, start_idx, end_idx, model):
+    all_results = []
     for idx in range(start_idx, end_idx + 1):
         item = data[idx]
         if item['question_id'] not in forbidden_list:
@@ -73,30 +72,28 @@ def process_data(data, start_idx, end_idx):
                 image_path = "./data/images/" + item["image"]
                 base64_image = encode_image(image_path)
                 
-                response = api_call(item["question"], base64_image)
-                
+                response = api_call(item["question"], base64_image, model)
                 resp_list = [unicodedata.normalize('NFKC', choice.message.content) for choice in response.choices]
                 item["responses"] = resp_list
             
                 logging.info(item)
-
-
-                # Save each item's result in a separate JSON file
-                with open(f'./gpt-call/gpt4o/output_{idx}-{question_id}.json', 'w', encoding='utf-8') as outfile:
+                all_results.append(item)
+                print(item)
+                with open(f'./eval/{model}/output_{idx}-{question_id}.json', 'w', encoding='utf-8') as outfile:
                     json.dump(item, outfile, ensure_ascii=False, indent=4)
+
             except Exception as e:
                 # Log the error
-                with open('./gpt-call/gpt4o/error_log.txt', 'a') as log_file:
-                    log_file.write(f'Error processing item at index {idx}: {str(e)}\n')
-                logging.info(e)
+                print("error_occured", e)
                 continue  # Skip to the next item
-
+    
 # Main function to manage data processing
 def main():
+    model="GPT-VLM-MODEL"
     data = load_data('./data/siuo_gen.json')
-    start_idx = 1  # Set this to the index from where you need to start or resume
+    start_idx = 0  # Set this to the index from where you need to start or resume
     end_idx = len(data) - 1  # Set this to the last index you want to process
-    process_data(data, start_idx, end_idx)
+    process_data(data, start_idx, end_idx, model)
 
 if __name__ == "__main__":
     main()
